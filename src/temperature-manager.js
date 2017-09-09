96d5d9cc8b335410c5956230158c090526eaa559
Kevin Dyer <kevin.m.dyer@gmail.com>
2017-09-09 08:39:59 -0600

    Removed device manager.

--- src/temperature-manager.js
+++ src/temperature-manager.js
@@ -1,10 +1,15 @@
 const EventEmitter = require('events');
 const five = require('johnny-five');
+const Raspi = require('raspi-io');
 const logger = require('./logger');
 
 class TemperatureManager extends EventEmitter {
   constructor() {
     super();
+    this._board = new five.Board({
+      io: new Raspi(),
+      repl: false
+    });
     this._thermometer = new five.Thermometer({
       controller: 'TMP102',
       freq: 10 * 1000
@@ -14,15 +19,19 @@ class TemperatureManager extends EventEmitter {
   }
 
   _onData(event) {
-    logger.debug('New temperature', event);
     this.emit('temp', event);
   }
 
   load() {
     return Promise.resolve()
     .then(() => {
-      this._thermometer.enable();
-    });
+      if (!this._board.isReady) {
+        return new Promise((resolve) => {
+          this._board.once('ready', resolve);
+        });
+      }
+    })
+    .then(() => this._thermometer.enable());
   }
 }
 
